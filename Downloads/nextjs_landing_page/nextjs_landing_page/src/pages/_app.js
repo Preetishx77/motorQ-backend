b85bae7c1d0eff21c438cfd9890d61f3b5e0e774
Preetish Biswal <78357366+Preetishx77@users.noreply.github.com>
2022-08-18 21:37:41 +0530

    Delete Downloads directory

--- Downloads/nextjs_landing_page/nextjs_landing_page/src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import 'react-multi-carousel/lib/styles.css';
-import 'react-modal-video/css/modal-video.min.css';
-import 'rc-drawer/assets/index.css';
-import 'typeface-dm-sans';
-
-export default function CustomApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
