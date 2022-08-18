73e345ef3a9150f228baad4eef26c3e3cb143bfd
Preetish Biswal <78357366+Preetishx77@users.noreply.github.com>
2022-08-18 21:38:02 +0530

    Delete blog-app directory

--- blog-app/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import PostReducer from "../redux/features/PostSlice";
-
-export default configureStore({
-  reducer: {
-    app: PostReducer,
-  },
-});
